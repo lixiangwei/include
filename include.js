3f44c028999b20dfa6ac8921bc3698a3907324be
cw <chuangweili@163.com>
2017-06-01 21:45:05 +0800

    Update include.js

--- include.js
+++ include.js
@@ -2,25 +2,6 @@
  *用法：
  * include.js([[url,callback], ...]);  include.js([url, ...]);
  * include.css([url]);
- *
- * 　　　┏┓　　　┏┓
- * 　　┏┛┻━━━┛┻┓
- * 　　┃　　　　　　　┃
- * 　　┃　　　━　　　┃
- * 　　┃　┳┛　┗┳　┃
- * 　　┃　　　　　　　┃
- * 　　┃　　　┻　　　┃
- * 　　┃　　　　　　　┃
- * 　　┗━┓　　　┏━┛Code is far away from bug with the animal protecting
- * 　　　　┃　　　┃    神兽保佑,代码无bug
- * 　　　　┃　　　┃
- * 　　　　┃　　　┗━━━┓
- * 　　　　┃　　　　　 ┣┓
- * 　　　　┃　　　　 ┏┛
- * 　　　　┗┓┓┏━┳┓┏┛
- * 　　　　　┃┫┫　┃┫┫
- * 　　　　　┗┻┛　┗┻┛
- *
  */
 var include = function (window) {
 
